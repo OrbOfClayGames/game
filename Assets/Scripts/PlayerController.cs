44c7676605bceaeb80bd480ddc69c2420e7fd4d4
OrbOfClayGames <60816649+OrbOfClayGames@users.noreply.github.com>
2021-04-02 09:27:35 +0100

    re-naming pass (assets, CharMovementConcept Script) added sorting layers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharMovementConcept : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // config parameters
     [SerializeField] float xVelocity = 3f;
