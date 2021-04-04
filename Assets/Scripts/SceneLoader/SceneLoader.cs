398689d49d218a062bf6a0c3688d2af7289af76f
OrbOfClayGames <60816649+OrbOfClayGames@users.noreply.github.com>
2021-04-04 18:18:47 +0100

    added trigger on collision with Player tag to load specified scene

--- Assets/Scripts/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -11,7 +11,10 @@ public class SceneLoader : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
 
-        SceneManager.LoadScene(sceneToLoad);
+        
     }    
 }
