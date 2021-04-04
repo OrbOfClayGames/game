6ad7468c18825505e4f3a8a92530200771fe56d0
OrbOfClayGames <60816649+OrbOfClayGames@users.noreply.github.com>
2021-04-04 18:15:36 +0100

    added Player tag to Player prefab, 2d collider exit area to Forest level, SceneLoader script

---
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities.Inspector;
+
+public class SceneLoader : MonoBehaviour
+{
+    public SceneField sceneToLoad;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+
+        SceneManager.LoadScene(sceneToLoad);
+    }    
+}
