2b419b278c9e44908d3e1cb337f4117d3899f85e
OrbOfClayGames <60816649+OrbOfClayGames@users.noreply.github.com>
2021-04-05 10:52:25 +0100

    specified Level Transitinoname in LevelExit gameObject to be the same ("Forest") to link transitions together correctly

--- Assets/Scripts/SceneLoader/SceneEntrance.cs
+++ Assets/Scripts/SceneLoader/SceneEntrance.cs
@@ -12,6 +12,7 @@ public class SceneEntrance : MonoBehaviour
         if (transitionName == PlayerController.instance.levelTransitionname)
         {
             PlayerController.instance.transform.position = transform.position;
+
             PlayerController.instance.GetComponent<Animator>().SetBool("walking", false);            
             PlayerController.instance.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
             PlayerController.instance.hasStarted = false;
