fe77ad26da3c123301821398a1cfc3494762e5ec
OrbOfClayGames <60816649+OrbOfClayGames@users.noreply.github.com>
2021-04-09 12:33:40 +0100

    refactored Animator and Rigidbody2D GetComponent<> into cached references

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // config parameters
-    [SerializeField] public float xVelocity = 1.5f;    
+    [SerializeField] public float xVelocity = 1.5f;
+
+    //cached references    
+    private Rigidbody2D cachedRigidBody2D;
+    private Animator cachedAnimator;
+
+    private void Awake()
+    {
+        cachedRigidBody2D = GetComponent<Rigidbody2D>();
+        cachedAnimator = GetComponent<Animator>();
+    }
 
     public void Walk()
     {        
-            Vector2 playerVelocity = new Vector2(xVelocity, 0f);
-            GetComponent<Rigidbody2D>().velocity = playerVelocity;        
-            GetComponent<Animator>().SetBool("walking", true);
+        Vector2 playerVelocity = new Vector2(xVelocity, 0f);        
+        cachedRigidBody2D.velocity = playerVelocity;        
+        cachedAnimator.SetBool("walking", true);
     }
 }
